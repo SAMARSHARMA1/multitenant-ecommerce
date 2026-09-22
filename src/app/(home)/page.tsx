0ea6466dd99a5b41ffeee9c5362cbe66fc0b5f47
SAMARSHARMA1 <mk9816785085@gmail.com>
2026-09-22 23:55:00 +0530

    03: home layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,9 @@ import { Input } from "@/components/ui/input";
 import { Progress } from "@/components/ui/progress";
 import { Textarea } from "@/components/ui/textarea";
 
+
 export default function Home() {
+  
   return(
 <div className="p-4">
 <div className="flex flex-col gap-y-4">
